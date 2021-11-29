acc02c7e14838170ff62a088fbfffcf11341e1c3
blockomat2100 <secret@example.com>
2021-11-29 18:12:12 +0100

    fix broken useradmin

--- account/admin.py
+++ account/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 from account import models
 
 
 class ProfileInline(admin.StackedInline):
     model = models.Profile
+    can_delete = False
+    max_num = 1
 
 
-class ExtendedUserAdmin(admin.ModelAdmin):
+class ExtendedUserAdmin(UserAdmin):
     inlines = [ProfileInline]
 
 
