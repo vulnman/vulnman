43059056a2863ef311691178621897d49768969b
blockomat2100 <secret@example.com>
2021-12-01 13:44:53 +0100

    closes #39

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Collect static files
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+python manage.py create_secret_key
+
+# create superuser
+echo "Create superuser"
+python manage.py createsuperuser --noinput
+
+# Start server
+echo "Starting server"
+gunicorn --bind 0.0.0.0:8000 vulnman.wsgi
