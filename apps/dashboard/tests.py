208feaa033751eb512101574311deea631535a6b
blockomat2100 <secret@example.com>
2021-12-02 08:25:46 +0100

    add dashboard views test

--- apps/dashboard/tests.py
+++ apps/dashboard/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from vulnman.tests.mixins import VulnmanTestMixin
 
-# Create your tests here.
+
+class ProjectDashboardTestCase(TestCase, VulnmanTestMixin):
+    def setUp(self) -> None:
+        self.init_mixin()
+
+    def test_status_code(self):
+        self._test_unauth_access("dashboard:dashboard")
