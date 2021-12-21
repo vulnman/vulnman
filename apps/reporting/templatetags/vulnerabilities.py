91b5bf493f5d61923df820b5f64610b4d0dfc624
blockomat2100 <secret@example.com>
2021-12-21 15:48:49 +0100

    split templates, vulnerabilities and their details into new models

--- apps/reporting/templatetags/vulnerabilities.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.simple_tag
-def call_method(obj, method_name, *args):
-    method = getattr(obj, method_name)
-    return method(*args)
