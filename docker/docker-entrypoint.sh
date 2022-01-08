c6f5b6d757977b864b9bcc331114c4139125e105
blockomat2100 <xzszz795duxv@riseup.net>
2022-01-08 12:16:09 +0100

    fix migrations

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Collect static files
-echo "Collect static files"
-python manage.py collectstatic --noinput
 
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate
+
+
+# Collect static files
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
 
 python manage.py create_secret_key
 
