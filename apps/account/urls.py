389e855a43a061e80a78ea7e6a9eaaaa610a7979
blockomat2100 <xzszz795duxv@riseup.net>
2022-02-28 20:02:06 +0100

    change password method

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -6,5 +6,6 @@ app_name = "account"
 urlpatterns = [
     path('login/', views.Login.as_view(), name="login"),
     path('logout/', views.Logout.as_view(), name="logout"),
-    path('profile/edit/', views.ProfileEdit.as_view(), name="profile-edit")
+    path('profile/', views.Profile.as_view(), name="profile-edit"),
+    path('change-password/', views.ChangePassword.as_view(), name="change-password")
 ]
--- apps/account/views.py
+++ apps/account/views.py
@@ -37,3 +37,18 @@ class ProfileEdit(generic.VulnmanAuthUpdateView):
             self.request.user.email = form.cleaned_data["email"]
             self.request.user.save()
         return super().form_valid(form)
+
+
+class Profile(generic.VulnmanAuthTemplateView):
+    template_name = "account/profile.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["change_password_form"] = forms.ChangePasswordForm(self.request.user)
+        return context
+
+
+class ChangePassword(ThemeMixin, views.PasswordChangeView):
+    form_class = forms.ChangePasswordForm
+    http_method_names = ["post"]
+    success_url = reverse_lazy("account:profile-edit")
