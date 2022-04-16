bb0879da2549e34cf8e86aafe5a8faf7e1d42f70
blockomat2100 <62061776+blockomat2100@users.noreply.github.com>
2022-04-16 18:22:12 +0200

    Download vuln templates (#123)
    * closes #120
    
    * update submodule
    
    * download vulnerability templates from repo
    
    Co-authored-by: blockomat2100 <secret@random.com>

--- vulnman/settings/orig.py
+++ vulnman/settings/orig.py
@@ -1,17 +1,17 @@
-import os
-from vulnman.settings import BASE_DIR
-
-
 VULNMAN_CSS_THEME = "vulnman-dark"
 
+# TODO: not used
 HOST_OS_ICONS = {
     "linux": {
-        "icon": "fa fa-linux", "matches": ["Ubuntu", "Fedora", "Arch-Linux", "Debian", "Linux"]
+        "icon": "fa fa-linux", "matches": [
+            "Ubuntu", "Fedora", "Arch-Linux", "Debian", "Linux"]
     }
 }
 
+# TODO: legacy stuff
 CUSTOM_EXTERNAL_TOOLS = {}
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
+VULNERABILITY_TEMPLATE_REPO = "https://github.com/vulnman/community-vulnerability-templates"
