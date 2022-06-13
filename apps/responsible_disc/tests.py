93a52bb056d69afe9048fe89d4457eb2d5d7d984
blockomat2100 <secret@random.com>
2022-06-13 06:56:00 +0200

    style improvement and link fixes

--- apps/responsible_disc/tests.py
+++ apps/responsible_disc/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from vulnman.tests.mixins import VulnmanTestMixin
+from apps.responsible_disc import models
+from apps.findings.models import Template
 
-# Create your tests here.
+
+class VulnerabilityListView(TestCase, VulnmanTestMixin):
+    def setUp(self) -> None:
+        self.init_mixin()
+
+    def test_listview_forbidden(self):
+        url = self.get_url("responsible_disc:vulnerability-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
+    def test_listview(self):
+        url = self.get_url("responsible_disc:vulnerability-list")
+        vuln1 = self._create_instance(models.Vulnerability, user=self.pentester1)
+        self._create_instance(models.Vulnerability, user=self.pentester2)
+        self.client.force_login(self.pentester1)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context["vulnerabilities"]), 1)
+        self.assertEqual(response.context["vulnerabilities"][0], vuln1)
+
+    def test_createview(self):
+        template = self._create_instance(Template)
+        url = self.get_url("responsible_disc:vulnerability-create")
+        payload = {"template_id": template.vulnerability_id, "name": "TestVuln",
+                   "status": models.Vulnerability.STATUS_OPEN, "vendor": "TestVendor",
+                   "vendor_homepage": "https://example.com", "vendor_email": "admin@example.com",
+                   "affected_product": "Test Product", "affected_versions": "<1.0.0",
+                   "severity": ""
+                   }
+        self.client.force_login(self.pentester1)
+        response = self.client.post(url, payload)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(models.Vulnerability.objects.count(), 1)
