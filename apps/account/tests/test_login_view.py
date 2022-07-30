b5d6c38c702ba3c20b04a327b1b11ce3e4058b68
blockomat2100 <secret@random.com>
2022-07-30 15:06:13 +0200

    fix tests

--- apps/account/tests/test_login_view.py
+++ apps/account/tests/test_login_view.py
@@ -10,21 +10,21 @@ class LoginViewBaseTestCase(TestCase, VulnmanTestCaseMixin):
         self.url = self.get_url("account:login")
 
     def test_login_pentester(self):
-        data = {"username": self.pentester1.username,
-                "password": "changeme"}
+        data = {"auth-username": self.pentester1.username,
+                "auth-password": "changeme", "login-current_step": "auth"}
         response = self.client.post(self.url, data)
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse(settings.LOGIN_REDIRECT_URL))
 
     def test_login_vendor(self):
         vendor = self._create_user("testvendor", "password", is_vendor=True)
-        data = {"username": vendor.username, "password": "password"}
+        data = {"auth-username": vendor.username, "auth-password": "password", "login-current_step": "auth"}
         response = self.client.post(self.url, data)
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse(settings.LOGIN_REDIRECT_URL))
 
     def test_login_wrong_creds(self):
-        data = {"username": "randomusername", "password": "randompassword"}
+        data = {"auth-username": "randomusername", "auth-password": "randompassword", "login-current_step": "auth"}
         response = self.client.post(self.url, data)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context.get('form').is_valid(), False)
--- vulnman/api/tests/testcase.py
+++
@@ -1,5 +0,0 @@
-from vulnman.core.test.testcase_mixin import VulnmanTestCaseMixin
-
-
-class VulnmanAPITestCaseMixin(VulnmanTestCaseMixin):
-    pass
